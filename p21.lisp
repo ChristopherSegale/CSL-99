810ad2cfab8f8545bd8c05ff05468957d8a5a1fd
ChristopherSegale <ccsp.segale@gmail.com>
2014-01-01 13:54:52 -0500

    restructuring master branch

--- p21.lisp
+++ p21.lisp
@@ -1,11 +1,7 @@
 ;;;; Author: Christopher Segale
 ;;;; Date: 01/01/2014
 
-(defun split (sequence length)
-  (let ((first-list '()) (rest-of-list sequence))
-    (dotimes (n length)
-      (push (pop rest-of-list) first-list))
-  (list (reverse first-list) rest-of-list)))
+(load "p17.lisp")
 
 (defun insert-at (obj sequence index)
   (let ((split-list (split sequence (1- index))))
